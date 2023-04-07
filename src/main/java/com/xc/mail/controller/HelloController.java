8ce42bf0da1a986cf58c0d75a2710fda7a1327ee
zhuweitao <zhuweitao@swordgate.cn>
2023-04-07 11:31:53 +0800

    999

--- src/main/java/com/xc/mail/controller/HelloController.java
+++ src/main/java/com/xc/mail/controller/HelloController.java
@@ -1,13 +1,28 @@
 package com.xc.mail.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+/**
+ * @author admin
+ */
 @RestController
 public class HelloController {
 
+   // private static final Logger logger = LoggerFactory.getLogger(HelloController.class);
+
+    private static final Logger logger = LoggerFactory.getLogger(HelloController.class);
+
+
     @RequestMapping("/hello")
-    public String hello(){
+    public String hello(@RequestBody User user){
+       // logger.debug("日志开始打印");
+        System.out.println(user.toString());
+        logger.info("日志开始");
+        logger.info("user:{}", user);
         return "Hello Docker";
     }
 }
