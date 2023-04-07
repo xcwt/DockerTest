3f00c75a60388ae2f887389916e0c50b782b310e
zhuweitao <zhuweitao@swordgate.cn>
2023-04-07 16:10:42 +0800

    999

---
+++ src/main/java/com/xc/mail/controller/User.java
@@ -0,0 +1,42 @@
+package com.xc.mail.controller;
+
+
+
+
+/**
+ * @Author：zwt
+ * @Project：DockerTest
+ * @Date：2023/4/7 10:47
+ * @Filename：User
+ */
+
+public class User {
+
+    private Integer id;
+
+    private String name;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                '}';
+    }
+}
